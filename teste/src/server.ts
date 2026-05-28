1036594bab58818277821b5787cba7a8e91b8cd2
Gabrielle Oliveira de Paula <gabrielleoliveirah14@gmail.com>
2026-05-27 21:24:10 -0300

    Delete teste directory

--- teste/src/server.ts
+++
@@ -1,9 +0,0 @@
-import "dotenv/config";
-import { createApp } from "./app.js";
-
-const app = createApp();
-const PORT = process.env.PORT ?? 3000;
-
-app.listen(PORT, () => {
-  console.log(`🚀 Task Manager API rodando em http://localhost:${PORT}`);
-});
